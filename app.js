0580b1621701ef4d50596b1dce87b25f66499b41
Paweł Kątny <pawel.katny@gmail.com>
2021-03-13 19:10:27 +0100

    First commit. Add basic BabylonJS scene.

---
+++ app.js
@@ -0,0 +1,17 @@
+const createScene = (engine, canvas) => {
+    const scene = new BABYLON.Scene(engine);
+    const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 3, new BABYLON.Vector3(0, 0, 0), scene);
+    camera.attachControl(canvas, true);
+    const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
+
+    return scene;
+}
+
+const canvas = document.getElementById('renderCanvas');
+const engine = new BABYLON.Engine(canvas, true);
+
+const scene = createScene(engine, canvas);
+
+engine.runRenderLoop(() => {
+    scene.render();
+})
