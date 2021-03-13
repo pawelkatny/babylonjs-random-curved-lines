b0f2a69046257f87bfa4cbbda57eb011a2131b4e
Paweł Kątny <pawel.katny@gmail.com>
2021-03-13 19:22:55 +0100

    Add basic ground.

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
+const createGround = (width, height) => {
+    const ground = new BABYLON.MeshBuilder.CreateGround('ground', {width: width, height: height});
+    return ground;
+}
+
 const createScene = (engine, canvas) => {
     const scene = new BABYLON.Scene(engine);
     const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 3, new BABYLON.Vector3(0, 0, 0), scene);
     camera.attachControl(canvas, true);
     const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
+    const ground = createGround(50, 1000);
 
     return scene;
 }
